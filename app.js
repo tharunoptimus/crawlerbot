c660acb6c3fc78066971546a371ea494bc9b5312
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-07-17 13:40:37 +0530

    Connect To Database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000;
 const path = require('path')
+const mongoose = require("./database")
 const server = app.listen(port, () => console.log("Server Listening on " + port));
 
 app.get("/", (req, res) => {
